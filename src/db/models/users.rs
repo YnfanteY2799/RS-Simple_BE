00efdc31a935da413bc55819bc867eb9d0a73cd8
Yassett Ynfante <49245097+YnfanteY2799@users.noreply.github.com>
2025-03-14 17:31:42 -0400

    +adds

--- src/db/models/users.rs
+++ src/db/models/users.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveDate;
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 
@@ -6,10 +7,7 @@ use crate::db::schema::schema::user;
 use crate::db::schema::schema::user_status;
 use crate::db::schema::schema::user_type;
 
-#[derive(Debug, Serialize, Deserialize, Queryable, Identifiable, Associations, PartialEq)]
-#[diesel(belongs_to(DocumentType, foreign_key = document_type_id))]
-#[diesel(belongs_to(UserType, foreign_key = user_type_id))]
-#[diesel(belongs_to(UserStatus, foreign_key = status_id))]
+#[derive(Debug, Serialize, Deserialize, Queryable, Identifiable, PartialEq)]
 #[diesel(table_name = crate::db::schema::schema::user)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 #[diesel(primary_key(id))]
@@ -66,35 +64,4 @@ pub struct UpdateUser {
     pub document_type_id: Option<i16>,
     pub status_id: Option<i16>,
     pub user_type_id: Option<i16>,
-}
-
-// Related entities for relationships
-#[derive(Debug, Serialize, Deserialize, Queryable, Identifiable, Associations)]
-#[diesel(table_name = document_type)]
-#[diesel(primary_key(id))]
-pub struct DocumentType {
-    pub id: i16,
-    pub name: String,
-    pub create_time: NaiveDate,
-    pub update_time: Option<NaiveDate>,
-}
-
-#[derive(Debug, Serialize, Deserialize, Queryable, Identifiable, Associations)]
-#[diesel(table_name = user_status)]
-#[diesel(primary_key(id))]
-pub struct UserStatus {
-    pub id: i16,
-    pub name: String,
-    pub created_at: NaiveDate,
-    pub updated_at: Option<NaiveDate>,
-}
-
-#[derive(Debug, Serialize, Deserialize, Queryable, Identifiable, Associations)]
-#[diesel(table_name = user_type)]
-#[diesel(primary_key(id))]
-pub struct UserType {
-    pub id: i16,
-    pub name: String,
-    pub created_at: NaiveDate,
-    pub updated_at: Option<NaiveDate>,
 }
--- src/db/repository/users.rs
+++ src/db/repository/users.rs
@@ -1,9 +1,6 @@
+use crate::db::{models::users::User, schema::schema::user::username};
 use diesel::prelude::*;
-use crate::db::models::users::User;
-use crate::db::schema::user::dsl::*;
 
-pub fn find_user_by_username(conn: &mut PgConnection, username_param: &str) -> QueryResult<User> {
-    user.filter(username.eq(username_param)).first(conn)
-}
+pub fn find_user_by_username(conn: &mut PgConnection, username_param: &str) {}
 
 // Create a logo for a Tech related company focused on the modern and constant technological improvements, the logo must contain a White Wolf or be White Wolf related, due to the company creed
